10386bb8cfbbf5a28f716db504441b49ce7a6233
aleksandr2639 <alexanderkriulin@yandex.ru>
2023-08-24 22:30:23 +0300

    add: Modal.js

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -12,7 +12,11 @@ class Modal {
    * необходимо выкинуть ошибку.
    * */
   constructor(element){
-
+    if(!element) {
+      throw new Error('Ошибка!')
+    }
+    this.element = element;
+    this.registerEvents();
   }
 
   /**
@@ -21,27 +25,29 @@ class Modal {
    * (с помощью метода Modal.onClose)
    * */
   registerEvents() {
-
+    const modal = Array.from(document.querySelectorAll('[data-dismiss]'));
+    modal.forEach((item) => item.addEventListener('click', (e) => this.onClose(e))
+    )
   }
 
   /**
    * Срабатывает после нажатия на элементы, закрывающие окно.
    * Закрывает текущее окно (Modal.close())
    * */
-  onClose(e) {
-
+  onClose() {
+    this.close();
   }
   /**
    * Открывает окно: устанавливает CSS-свойство display
    * со значением «block»
    * */
   open() {
-
+   this.element.style.display ='block';
   }
   /**
    * Закрывает окно: удаляет CSS-свойство display
    * */
   close(){
-
+    this.element.style.display ='';
   }
 }
