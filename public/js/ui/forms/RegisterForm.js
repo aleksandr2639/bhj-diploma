956a01ed2566fa9e752ee5cfc3b59a51273c2589
aleksandr2639 <alexanderkriulin@yandex.ru>
2023-08-24 22:32:09 +0300

    add: RegisterForm.js

--- public/js/ui/forms/RegisterForm.js
+++ public/js/ui/forms/RegisterForm.js
@@ -10,6 +10,13 @@ class RegisterForm extends AsyncForm {
    * и закрывает окно, в котором находится форма
    * */
   onSubmit(data) {
-
+    User.register(data, (response) => {
+      if (response && response.success) {
+        App.setState('user-logged');
+        App.getModal('register').close();
+        this.element.reset();
+      }
+    })
   }
+
 }
