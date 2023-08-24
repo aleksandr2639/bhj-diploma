13eaa93f570da834cab2ec02184fd8b0a4f99cbc
aleksandr2639 <alexanderkriulin@yandex.ru>
2023-08-24 22:29:44 +0300

    add: createRequest.js

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,38 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+    const xhr = new XMLHttpRequest(),
+          arr =[],
+          formData = new FormData();
+    let url;
 
+    try {
+        if( options.method === 'GET') {
+            for (const [key, value] of Object.entries(options.data)) {
+                arr.push(`${key} = ${value}`)
+            }
+            url = `${options.url}?${arr.join('&')}`;
+            xhr.open(options.method, url);
+            xhr.send();
+        } else {
+            for (const [key, value] of Object.entries(options.data)) {
+                formData.append(key, value);
+            }
+            url = `${options.url}`;
+            xhr.open(options.method, url);
+            xhr.send(formData);
+        }
+        xhr.responseType = 'json';
+
+        xhr.onload = () => {
+            if (xhr.status < 400) {
+                options.callback(xhr.error, xhr.response);
+            } else {
+                console.error(`Ошибка ${xhr.status}: ${xhr.statusText}.`);
+            }
+        }
+    }
+    catch (e) {
+        options.callback(e);
+    }
 };
